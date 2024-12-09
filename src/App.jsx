72db2351a19f47ff9f250ad2da075bfec1038818
Adrian <serbanedward@yahoo.com>
2024-12-09 11:04:49 +0100

    register created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar.jsx";
+import Registration from "./components/registration/Registration.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Registration />
     </>
   );
 }
--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Registration = () => {
-  return (
-    <div>Registration</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Registration
+  const [details, setDetails] = useState([]);
+  const userData = { name, email, password };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        userData
+      );
+      setDetails(response.data);
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form style={{ marginTop: "30px" }}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button
+          style={{
+            border: "1px solid black",
+            borderRadius: " 5px ",
+            width: "80px",
+          }}
+          type="submit"
+          value="Submit"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
